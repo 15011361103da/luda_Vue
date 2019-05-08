a1fac2918834e514a929a7777057e04bfd6bd668
15011361103da <luda395353765@163.com>
2019-05-08 16:23:13 +0800

    less

---
+++ src/components/luda.vue
@@ -0,0 +1,15 @@
+<template>
+    <div >
+      12366666666666666
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "luda"
+    }
+</script>
+
+<style scoped>
+  @import "./css/luda.less";
+</style>
