88fc0762550665b0b136a04397314bd973811ef4
15011361103da <luda395353765@163.com>
2019-05-14 23:13:20 +0800

    sdfzc

---
+++ src/components/v-for.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="(name,index,key) in names" v-bind:key="index">{{name}}-{{index}}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "v-for",
+      data(){
+        return{
+          names:[
+            'lda  ',
+            'xiuyuchuan',
+            'lujianhong',
+            'hangjunyying'
+          ]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import luda from '../components/luda'
 import iconfontTest from '../components/iconfontTest'
+import vFor from '../components/v-for'
 
 
 
@@ -25,6 +26,11 @@ export default new Router({
       path: '/iconfontTest',
       name: 'iconfontTest',
       component: iconfontTest
-    }
+    },
+    {
+      path: '/vFor',
+      name: 'vFor',
+      component: vFor
+    },
   ]
 })
