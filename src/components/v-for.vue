d55027983da439a8237ca1fc943f311288061fac
15011361103da <luda395353765@163.com>
2019-05-16 10:23:18 +0800

    v-for的key作用

--- src/components/v-for.vue
+++ src/components/v-for.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
+      <div>
+        <label>Id:
+          <input type="text" v-model="id">
+        </label>
+        <label>Name:
+          <input type="text" v-model="name">
+        </label>
+        <input type="button" value="添加" @click="add">
+      </div>
       <ul>
-        <li v-for="(name,index,key) in names" v-bind:key="index">{{name}}-{{index}}</li>
+        <li v-for="item in list" :key="item.id">
+          <input type="checkbox">
+          {{item.id}}---{{item.name}}
+        </li>
       </ul>
     </div>
 </template>
@@ -11,17 +23,26 @@
       name: "v-for",
       data(){
         return{
-          names:[
-            'lda  ',
-            'xiuyuchuan',
-            'lujianhong',
-            'hangjunyying'
-          ]
+          id:'',
+          name:'',
+          list:[
+            {id:1,name:'李斯'},
+            {id:2,name:'嬴政'},
+            {id:3,name:'赵高'},
+            {id:4,name:'韩非'},
+            {id:5,name:'荀子'}
+          ],
         }
-      }
+      },
+      methods:{
+        add(){
+          //this.list.push({id:this.id,name:this.name});
+          this.list.unshift({id:this.id,name:this.name});
+        }
+      },
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
