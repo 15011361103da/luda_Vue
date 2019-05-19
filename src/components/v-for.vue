e18c37885da83de69bf2dd4a4e943b31096f8474
15011361103da <luda395353765@163.com>
2019-05-19 12:28:27 +0800

    vfor

--- src/components/v-for.vue
+++ src/components/v-for.vue
@@ -15,6 +15,10 @@
           {{item.id}}---{{item.name}}
         </li>
       </ul>
+
+      <p class="shop">
+        士大夫空间和内饰的烦恼事的方兰看看
+      </p>
     </div>
 </template>
 
@@ -26,7 +30,7 @@
           id:'',
           name:'',
           list:[
-            {id:1,name:'李斯'},
+            {id:7,name:'李斯'},
             {id:2,name:'嬴政'},
             {id:3,name:'赵高'},
             {id:4,name:'韩非'},
@@ -44,5 +48,10 @@
 </script>
 
 <style>
-
+  .shop{
+    width: 160px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 </style>
